f25956bc42ae86bb8f80e899b256270c405ed5f1
Zheng, Lei <realthunder.dev@gmail.com>
2024-01-07 11:58:18 +0800

    PD: export 'Linearize' option to task panel
    For feature Pad/Pocket/Pipe/Loft

--- src/Mod/PartDesign/Gui/TaskPipeParameters.cpp
+++ src/Mod/PartDesign/Gui/TaskPipeParameters.cpp
@@ -96,6 +96,9 @@ TaskPipeParameters::TaskPipeParameters(ViewProviderPipe *PipeView, bool /*newObj
         ui->checkBoxRotateProfile->setToolTip(QString::fromUtf8(doc));
 
     this->initUI(proxy);
+    if (vp && vp->getObject()) {
+        hookPropertyBool(vp->getObject(), "Linearize", ui->checkBoxLinearize, "Linearize");
+    }
     this->groupLayout()->addWidget(proxy);
 
     refresh();
