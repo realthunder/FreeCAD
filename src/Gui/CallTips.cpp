23760a3d397bc46d5313923065ea49d3ebd86397
Zheng, Lei <realthunder.dev@gmail.com>
2024-02-11 18:11:44 +0800

    Gui: fix python console lost of bracket input on completion

--- src/Gui/CallTips.cpp
+++ src/Gui/CallTips.cpp
@@ -698,7 +698,6 @@ bool CallTipsList::eventFilter(QObject * watched, QEvent * event)
             }
             else if (this->compKeys.indexOf(ke->key()) > -1) {
                 Q_EMIT itemActivated(currentItem());
-                return false;
             }
             else if (ke->key() == Qt::Key_Shift || ke->key() == Qt::Key_Control ||
                      ke->key() == Qt::Key_Meta || ke->key() == Qt::Key_Alt ||
