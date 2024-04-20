bb54a0b6144a187f8dddd4aa4573ed0914096de8
Zheng, Lei <realthunder.dev@gmail.com>
2024-04-20 15:25:44 +0800

    Sketcher: fix constraint pre selection highlight
    Fixes #992

--- src/Mod/Sketcher/Gui/ViewProviderSketch.cpp
+++ src/Mod/Sketcher/Gui/ViewProviderSketch.cpp
@@ -3663,9 +3663,6 @@ void ViewProviderSketch::updateColor(void)
 
     auto setConstraintColors = [&](int i, const SbColor *highlightColor) {
         SoSeparator *s = static_cast<SoSeparator *>(edit->constrGroup->getChild(i));
-        if (s->getNumChildren() == 0) {
-            return;
-        }
 
         // Check Constraint Type
         Sketcher::Constraint* constraint = getSketchObject()->Constraints.getValues()[i];
@@ -4144,9 +4141,6 @@ void ViewProviderSketch::drawConstraintIcons()
         SoSeparator *sep = static_cast<SoSeparator *>(edit->constrGroup->getChild(constrId));
         int numChildren = sep->getNumChildren();
         if (numChildren <= CONSTRAINT_SEPARATOR_INDEX_FIRST_CONSTRAINTID) {
-#ifdef FC_DEBUG
-            FC_ERR("Invalid constraint node " << constrId);
-#endif
             break;
         }
 
