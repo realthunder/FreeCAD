1b11766fec08ea34581eb03d34293257da793521
Mihail Dumitrescu <mihai.dumitresq@gmail.com>
2024-05-29 16:48:30 +0300

    Add ParallelPlane attachment mode.
    It results in an attachment similar to ObjectXY but with the XY plane translated to pass through a selected vertex.
    
    It is most useful to place sketches: pick a plane (XY, XZ, YZ) or another sketch then select a vertex to automatically translate the sketch in the Z-direction. In contrast to the Translate mode it does not change the origin.

--- src/Mod/Part/App/Attacher.h
+++ src/Mod/Part/App/Attacher.h
@@ -110,6 +110,8 @@ enum eMapMode {
     mmOYZ,
     mmOYX,
 
+    mmParallelPlane,
+
     mmDummy_NumberOfModes//a value useful to check the validity of mode value
 };//see also eMapModeStrings[] definition in .cpp
 
