c145c3fd285856ec398b1eb9d9546c3054fe3933
Zheng, Lei <realthunder.dev@gmail.com>
2024-06-22 12:11:48 +0800

    Gui: fix preselection in "Pick geometry" menu

--- src/Gui/SelectionView.cpp
+++ src/Gui/SelectionView.cpp
@@ -952,7 +952,7 @@ bool SelectionMenu::eventFilter(QObject *o, QEvent *ev)
 {
     switch(ev->type()) {
     case QEvent::Show: {
-        Gui::Selection().rmvPreselect();
+        // Gui::Selection().rmvPreselect();
         break;
     }
     case QEvent::MouseButtonRelease: {
