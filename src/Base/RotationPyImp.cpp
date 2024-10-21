4efc973ce6cd4e3f98758247cbf976e8f5289997
Zheng, Lei <realthunder.dev@gmail.com>
2024-10-21 10:38:39 +0800

    Base: accept quanity with unit in RotationPy.Angle

--- src/Base/RotationPyImp.cpp
+++ src/Base/RotationPyImp.cpp
@@ -26,6 +26,7 @@
 #include <Base/GeometryPyCXX.h>
 #include <Base/Tools.h>
 #include <Base/PyWrapParseTupleAndKeywords.h>
+#include <Base/QuantityPy.h>
 
 // inclusion of the generated files (generated out of RotationPy.xml)
 #include "RotationPy.h"
@@ -512,7 +513,7 @@ void RotationPy::setAxis(Py::Object arg)
     this->getRotationPtr()->setValue(axis, angle);
 }
 
-Py::Float RotationPy::getAngle() const
+Py::Object RotationPy::getAngle() const
 {
     Base::Vector3d axis;
     double angle {};
@@ -520,12 +521,23 @@ Py::Float RotationPy::getAngle() const
     return Py::Float(angle);
 }
 
-void RotationPy::setAngle(Py::Float arg)
+void RotationPy::setAngle(Py::Object arg)
 {
     Base::Vector3d axis;
     double angle {};
     this->getRotationPtr()->getRawValue(axis, angle);
-    angle = static_cast<double>(arg);
+    if(!PyObject_TypeCheck(arg.ptr(),&Base::QuantityPy::Type)) {
+        if (!arg.isNumeric()) {
+            throw Py::TypeError("expect object of type float or quantity");
+        }
+        angle = static_cast<double>(Py::Float(arg));
+    }
+    else {
+        auto q = *static_cast<Base::QuantityPy*>(arg.ptr())->getQuantityPtr();
+        if (q.getUnit() != Base::Unit::Angle)
+            throw Py::TypeError("expect quantity of unit angle");
+        angle = Base::toRadians(q.getValue());
+    }
     this->getRotationPtr()->setValue(axis, angle);
 }
 
