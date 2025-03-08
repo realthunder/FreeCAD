e9b067c064187ba5321facf818957a865f3c012e
Florian Reimair <office@florianreimair.at>
2025-03-08 03:13:32 +0100

    [TechDraw] Fix clip group issue (#716)

--- src/Mod/TechDraw/Gui/Command.cpp
+++ src/Mod/TechDraw/Gui/Command.cpp
@@ -861,7 +861,8 @@ void CmdTechDrawClipGroup::activated(int iMsg)
     std::string FeatName = getUniqueObjectName("Clip",page);
     openCommand(QT_TRANSLATE_NOOP("Command", "Create Clip"));
     Gui::cmdAppDocument(page, std::ostringstream() << "addObject('TechDraw::DrawViewClip','" << FeatName << "')");
-    Gui::cmdAppObjectArgs(page, "addView(%s))", getObjectCmd(page));
+    auto feat = page->getDocument()->getObject(FeatName.c_str());
+    Gui::cmdAppObjectArgs(page, "addView(%s))", getObjectCmd(feat));
     updateActive();
     commitCommand();
 }
@@ -930,7 +931,7 @@ void CmdTechDrawClipGroupAdd::activated(int iMsg)
 
     openCommand(QT_TRANSLATE_NOOP("Command", "ClipGroupAdd"));
     Gui::cmdAppObjectHide(view);
-    Gui::cmdAppObject(pageClip, std::ostringstream() << "addView(" << getObjectCmd(view) << ")");
+    Gui::cmdAppObject(clip, std::ostringstream() << "addView(" << getObjectCmd(view) << ")");
     Gui::cmdAppObjectShow(view);
     updateActive();
     commitCommand();
